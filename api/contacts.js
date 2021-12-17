32bdb1040e3df63cc8d2cb2a80361ab70530265c
grahmnic <grahmnic@gmail.com>
2021-12-16 20:24:24 -0500

    fix

--- api/contacts.js
+++ api/contacts.js
@@ -1,8 +1,6 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
 const db = require('./models');
 
-export default (req: VercelRequest, res: VercelResponse) => {
+export default (req, res) => {
   db.Contact.findAll()
     .then((contacts) => res.send(contacts))
     .catch((err) => {
